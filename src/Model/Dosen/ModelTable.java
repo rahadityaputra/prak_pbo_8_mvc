aa4442a2d59ce488cff2c3d6ea6f99d603140063
rahadityaputra <rahadit.a.p123@gmail.com>
2025-05-21 15:32:27 +0700

    first int

---
+++ src/Model/Dosen/ModelTable.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Dosen;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author rafli
+ */
+public class ModelTable extends AbstractTableModel {
+
+    // Berfungsi untuk menyimpan daftar mahasiswa
+    List<ModelDosen> daftarDosen;
+
+    /*
+      Nama kolom tabelnya disimpan ke dalam variabel "namaKolom" yang memiliki 
+      tipe data Array String.
+     */
+    String kolom[] = {"ID", "Nama", "Nidn"};
+
+    /*
+      Karena daftarMahasiswa memiliki tipe data List, kita harus mengubahnya
+      terlebih dahulu ke dalam tipe data Array Object supaya dapat 
+      dimasukkan ke dalam table.
+     */
+    public ModelTable(List<ModelDosen> daftarDosen) {
+        this.daftarDosen = daftarDosen;
+    }
+
+    // Method untuk mengambil jumlah baris dari tabel
+    @Override
+    public int getRowCount() {
+        return daftarDosen.size();
+    }
+
+    // Method untuk mengambil jumlah kolom dari tabel
+    @Override
+    public int getColumnCount() {
+        return kolom.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        int ci = columnIndex;
+        switch (ci) {
+            case 0:
+                return daftarDosen.get(rowIndex).getId();
+            case 1:
+                return daftarDosen.get(rowIndex).getNama();
+            case 2:
+                return daftarDosen.get(rowIndex).getNidn();
+            default:
+                return null;
+        }
+    }
+    
+    @Override
+    public String getColumnName(int columnIndex) {
+        return kolom[columnIndex];
+    }
+}
